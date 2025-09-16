b1c95f64eeaced1bb8582abdc1259ce195ddd36b
sidwecode01 <sidoine.nango@epitech.eu>
2025-09-16 04:24:10 +0200

    [ADD]: register api route

---
+++ backend/OAuth2_app/app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Validator;
+        use App\Models\User;
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $request){
+       $validator = Validator::make($request->all(), [
+        "name" => "required",
+        "email" => "required|email",
+        "password" => "required",
+        "confirm_password" => "required|same:password"
+       ]);
+
+       if ($validator->fails()){
+        return response()->json([
+            "status" => 0,
+            "message" => "validation error",
+            "data" => $validator->errors()->all()
+        ]);
+       }
+
+       $user = User::create([
+        "name"=>$request->name,
+        "email" =>$request->email,
+        "password" => bcrypt($request->password)
+       ]);
+
+       $response = [];
+       $response["token"] = $user->createToken("MyApp")->plainTextToken;
+       $response["name"] = $user->name;
+       $response["email"] = $user->email;
+
+       return response()->json([
+        "status" => 1,
+        "message" => "user registered",
+        "data"=> $response
+       ]);
+    }
+}
